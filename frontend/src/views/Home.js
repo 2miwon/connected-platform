8b13fd55f1597a167fbbde4a3501cb85429d2984
2miwon <zhengsfsf@gmail.com>
2024-06-07 23:53:25 +0900

    frontend: backend call example

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -8,7 +8,7 @@ import $L from '@enact/i18n/$L';
 import {useConfigs} from '../hooks/configs';
 import {usePopup} from './HomeState';
 import {InputField} from '@enact/sandstone/Input';
-import {fetchAllVideos, debug} from '../hooks/fetch';
+import {fetchAllVideos} from '../hooks/fetch';
 
 
 import {useVideoTime} from './HomeState';
@@ -30,6 +30,7 @@ import Input from '@enact/sandstone/Input';
 const Home = () => {
 	const data = useConfigs();
 	const {isPopupOpen, handlePopupOpen, handlePopupClose} = usePopup();
+	const [videos, setVideos] = useState([]);
 	const [state, setState] = useState({
 		name: '',
 		filterType: 'Order from A~Z'
@@ -41,32 +42,43 @@ const Home = () => {
 		return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
 	};
 
-	const videos = [
-    { text: 'Biotech', src: 'https://videos.pexels.com/video-files/3195394/3195394-uhd_3840_2160_25fps.mp4' },
-    { text: 'VR Headset', src: 'https://videos.pexels.com/video-files/3209828/3209828-uhd_3840_2160_25fps.mp4' },
-    { text: 'Blood Sample', src: 'https://videos.pexels.com/video-files/4074364/4074364-hd_1280_720_25fps.mp4' },
-    { text: 'Tattoo', src: 'https://videos.pexels.com/video-files/4124030/4124030-uhd_4096_2160_25fps.mp4' },
-    { text: 'Clinic', src: 'https://videos.pexels.com/video-files/4488804/4488804-uhd_3840_2160_25fps.mp4' }
-  ];
-  	console.log("DSD", fetchAllVideos())
-  	console.log("debug", debug())
+	useEffect(() => {
+		async function fetchData() {
+		  try {
+			const videosData = await fetchAllVideos();
+			setVideos(videosData);
+		  } catch (error) {
+			console.error(error);
+		  }
+		}
+		fetchData();
+	  }, []);
+
+// 	const videos = [
+//     { text: 'Biotech', src: 'https://videos.pexels.com/video-files/3195394/3195394-uhd_3840_2160_25fps.mp4' },
+//     { text: 'VR Headset', src: 'https://videos.pexels.com/video-files/3209828/3209828-uhd_3840_2160_25fps.mp4' },
+//     { text: 'Blood Sample', src: 'https://videos.pexels.com/video-files/4074364/4074364-hd_1280_720_25fps.mp4' },
+//     { text: 'Tattoo', src: 'https://videos.pexels.com/video-files/4124030/4124030-uhd_4096_2160_25fps.mp4' },
+//     { text: 'Clinic', src: 'https://videos.pexels.com/video-files/4488804/4488804-uhd_3840_2160_25fps.mp4' }
+//   ];
+
   	const filterOptions = ['Order from A~Z', 'Order from Z~A', 'Order by Position', 'Reverse Order by Position'];
 
 
 	  const filteredVideos = videos
-    .filter(video => video.text.toLowerCase().includes(state.name.toLowerCase()))
-    .sort((a, b) => {
-      if (state.filterType === 'Order from A~Z') {
-        return a.text.localeCompare(b.text);
-      } else if (state.filterType === 'Order from Z~A') {
-        return b.text.localeCompare(a.text);
-      } else if (state.filterType === 'Order by Position') {
-        return videos.indexOf(a) - videos.indexOf(b);
-      } else if (state.filterType === 'Reverse Order by Position') {
-        return videos.indexOf(b) - videos.indexOf(a);
-      }
-      return 0;
-    });
+    .filter(video => video.Title.toLowerCase().includes(state.name.toLowerCase()))
+    // .sort((a, b) => {
+    //   if (state.filterType === 'Order from A~Z') {
+    //     return a.text.localeCompare(b.text);
+    //   } else if (state.filterType === 'Order from Z~A') {
+    //     return b.text.localeCompare(a.text);
+    //   } else if (state.filterType === 'Order by Position') {
+    //     return videos.indexOf(a) - videos.indexOf(b);
+    //   } else if (state.filterType === 'Reverse Order by Position') {
+    //     return videos.indexOf(b) - videos.indexOf(a);
+    //   }
+    //   return 0;
+    // });
 
 	return (
 		<>
@@ -97,8 +109,8 @@ const Home = () => {
 			<div className={css.mediaContainer}>
 
 			  {filteredVideos.map((video, index) => (
-          <MediaOverlay key={index} text={video.text} loop>
-            <source src={video.src} />
+          <MediaOverlay key={index} text={video.Title} loop>
+            <source src={video.URL} />
           </MediaOverlay>
         ))}
 			  
