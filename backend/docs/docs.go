0050d7b9cb498b624e3b28286af723d909d5f971
2miwon <zhengsfsf@gmail.com>
2024-06-08 03:31:24 +0900

    backend: swag config

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -19,12 +19,15 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
-	Host:             "",
-	BasePath:         "",
-	Schemes:          []string{},
-	Title:            "",
-	Description:      "",
+	Version:          "1.0",
+	Host:             "3.36.212.250:3000",
+	BasePath:         "/docs",
+	Schemes:          []string{
+		"http",
+		"https",
+	},
+	Title:            "SuperNova API Docs",
+	Description:      "This is a sample server for SuperNova API.",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
